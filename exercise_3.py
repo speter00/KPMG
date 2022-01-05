c09bdb8176886442f31e7bc074e6bc4b8b2d7f6e
Sánta Péter <santapeter00@gmail.com>
2022-01-05 16:26:06 +0100

    Finished exercise 3

---
+++ exercise_3.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def chess_board_maker(n: int) -> np.array:
+    """
+    Make a chessboard-like matrix.
+
+    :param n: size of the matrix
+    :return: the chessboard-like matrix
+    """
+    matrix_flat = np.ones(n * n)
+    matrix_flat[::2] *= -1  # this is the "base" for the matrix, it looks something like this: [1, -1, 1, -1, 1, -1...]
+
+    # if n is even, we have to multiply the matrix with its own transpose,
+    # if n is odd, the chessboard is almost ready, but the numbers are "inverted", so multiply by -1
+    return np.reshape(np.array(matrix_flat), (n, n)).transpose() * np.reshape(np.array(matrix_flat), (n, n)) \
+        if n % 2 == 0 else np.reshape(np.array(matrix_flat), (n, n)) * -1
