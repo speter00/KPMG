ec31fa8eb9cc7db1ff3263c49fd62277d952828d
Sánta Péter <santapeter00@gmail.com>
2022-01-06 16:52:16 +0100

    Added error handling to exercise 2, and simplified slightly

--- exercise_2.py
+++ exercise_2.py
@@ -25,25 +25,30 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
     # careful with the indexing, again
     # "fixing" coordinates of obstacles where it's relevant
 
-    # obstacles on the queen's row
-    row_obstacles = [(obstacle[0] - 1, obstacle[1] - 1) for obstacle in obstacles if obstacle[0] - 1 == queen_pos[0]]
+    row_obstacles = []  # obstacles on the queen's row
+    col_obstacles = []  # obstacles on the queen's column
+    desc_diagonal_obstacles = []  # obstacles on the same \ shaped diagonal as the queen
+    asc_diagonal_obstacles = []  # obstacles on the same / shaped diagonal as the queen
 
-    # obstacles on the queen's column
-    col_obstacles = [(obstacle[0] - 1, obstacle[1] - 1) for obstacle in obstacles if obstacle[1] - 1 == queen_pos[1]]
+    for obstacle in obstacles:
+        if obstacle[0] - 1 == queen_pos[0]:  # row index match
+            row_obstacles.append((obstacle[0] - 1, obstacle[1] - 1))
+        if obstacle[1] - 1 == queen_pos[1]:  # col index match
+            obstacle = (obstacle[0] - 1, obstacle[1] - 1)
+            if obstacle in row_obstacles:  # if both row and column of obstacle matches the queen's, raise an error
+                raise Exception("The obstacle can't be on the queen's field!")
+            col_obstacles.append(obstacle)
 
-    # obstacle is on the same \ shaped diagonal as the queen, if:
-    # the sum of the coordinates matches
+        # obstacle is on the same \ shaped diagonal as the queen, if:
+        # the sum of the coordinates matches
+        if (obstacle[0] - 1) + (obstacle[1] - 1) == queen_cor_sum:
+            desc_diagonal_obstacles.append((obstacle[0] - 1, obstacle[1] - 1))
 
-    desc_diagonal_obstacles = [(obstacle[0] - 1, obstacle[1] - 1) for obstacle in obstacles if
-                               (obstacle[0] - 1) + (obstacle[1] - 1) == queen_cor_sum]
-
-    # obstacle is on the same / shaped diagonal as the queen, if:
-    # the absolute difference of the coordinates matches
-
-    asc_diagonal_obstacles = [(obstacle[0] - 1, obstacle[1] - 1) for obstacle in obstacles if
-                              abs((obstacle[0] - 1) - (obstacle[1] - 1)) == queen_cor_diff
-                              # if both conditions are true, it's on the \ diagonal
-                              and (obstacle[0] - 1, obstacle[1] -1) not in desc_diagonal_obstacles]
+        # obstacle is on the same / shaped diagonal as the queen, if:
+        # the absolute difference of the coordinates matches...
+        # and the sum of coordinates doesn't match
+        elif abs((obstacle[0] - 1) - (obstacle[1] - 1)) == queen_cor_diff:
+            asc_diagonal_obstacles.append((obstacle[0] - 1, obstacle[1] - 1))
 
     if row_obstacles:  # if there is at least one obstacle on the same row as the queen
         # check fields to the right
@@ -51,7 +56,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         right_obstacle_cols = [obstacle[1] for obstacle in row_obstacles if obstacle[1] > queen_pos[1]]
         if right_obstacle_cols:  # if there is at least one obstacle to the right:
             closest_obstacle_col = min(right_obstacle_cols)
-            fields += ((closest_obstacle_col - queen_pos[1])-1)  # add number of fields between queen and obstacle
+            fields += ((closest_obstacle_col - queen_pos[1]) - 1)  # add number of fields between queen and obstacle
         else:
             fields += queen_right_border_distance  # add number of fields between queen and right side border
 
@@ -60,7 +65,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         left_obstacle_cols = [obstacle[1] for obstacle in row_obstacles if obstacle[1] < queen_pos[1]]
         if left_obstacle_cols:  # if there is at least one obstacle to the left:
             closest_obstacle_col = max(left_obstacle_cols)
-            fields += ((queen_pos[1] - closest_obstacle_col)-1)  # add number of fields between queen and obstacle
+            fields += ((queen_pos[1] - closest_obstacle_col) - 1)  # add number of fields between queen and obstacle
         else:
             fields += queen_left_border_distance  # add number of fields between queen and left side border
             # (this is equal to queen column coordinate)
@@ -73,7 +78,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         above_obstacle_rows = [obstacle[0] for obstacle in col_obstacles if obstacle[0] > queen_pos[0]]
         if above_obstacle_rows:  # if there is at least one obstacle above:
             closest_obstacle_row = min(above_obstacle_rows)
-            fields += ((closest_obstacle_row - queen_pos[0])-1)  # add number of fields between queen and obstacle
+            fields += ((closest_obstacle_row - queen_pos[0]) - 1)  # add number of fields between queen and obstacle
         else:
             fields += queen_upper_border_distance  # add number of fields between queen and upper border
 
@@ -82,7 +87,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         below_obstacle_rows = [obstacle[0] for obstacle in col_obstacles if obstacle[0] < queen_pos[0]]
         if below_obstacle_rows:  # if there is at least one obstacle below:
             closest_obstacle_row = max(below_obstacle_rows)
-            fields += ((queen_pos[0] - closest_obstacle_row)-1)  # add number of fields between queen and obstacle
+            fields += ((queen_pos[0] - closest_obstacle_row) - 1)  # add number of fields between queen and obstacle
         else:
             fields += queen_lower_border_distance  # add number of fields between queen and lower border
             # (this is equal to the queen row coordinate)
@@ -95,7 +100,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         above_obstacle_rows = [obstacle[0] for obstacle in desc_diagonal_obstacles if obstacle[0] > queen_pos[0]]
         if above_obstacle_rows:  # if there is at least one obstacle above:
             closest_obstacle_row = min(above_obstacle_rows)
-            fields += ((closest_obstacle_row - queen_pos[0])-1)  # add number of fields between queen and obstacle
+            fields += ((closest_obstacle_row - queen_pos[0]) - 1)  # add number of fields between queen and obstacle
         else:
             # add number of fields between queen and upper border or left border, whichever is closer
             fields += min(queen_upper_border_distance, queen_left_border_distance)
@@ -105,7 +110,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         below_obstacle_rows = [obstacle[0] for obstacle in desc_diagonal_obstacles if obstacle[0] < queen_pos[0]]
         if below_obstacle_rows:  # if there is at least one obstacle below:
             closest_obstacle_row = max(below_obstacle_rows)
-            fields += ((queen_pos[0] - closest_obstacle_row)-1)  # add number of fields between queen and obstacle
+            fields += ((queen_pos[0] - closest_obstacle_row) - 1)  # add number of fields between queen and obstacle
         else:
             # add number of fields between queen and lower border or right border, whichever is closer
             fields += min(queen_lower_border_distance, queen_right_border_distance)
@@ -120,7 +125,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         above_obstacle_rows = [obstacle[0] for obstacle in asc_diagonal_obstacles if obstacle[0] > queen_pos[0]]
         if above_obstacle_rows:  # if there is at least one obstacle above:
             closest_obstacle_row = min(above_obstacle_rows)
-            fields += ((closest_obstacle_row - queen_pos[0])-1)  # add number of fields between queen and obstacle
+            fields += ((closest_obstacle_row - queen_pos[0]) - 1)  # add number of fields between queen and obstacle
         else:
             # add number of fields between queen and upper border or right border, whichever is closer
             fields += min(queen_upper_border_distance, queen_right_border_distance)
@@ -130,7 +135,7 @@ def queen_moves_num(n: int, r_q: int, c_q: int, obstacles: List[Tuple[int, int]]
         below_obstacle_rows = [obstacle[0] for obstacle in asc_diagonal_obstacles if obstacle[0] < queen_pos[0]]
         if below_obstacle_rows:  # if there is at least one obstacle below:
             closest_obstacle_row = max(below_obstacle_rows)
-            fields += ((queen_pos[0] - closest_obstacle_row)-1)  # add number of fields between queen and obstacle
+            fields += ((queen_pos[0] - closest_obstacle_row) - 1)  # add number of fields between queen and obstacle
         else:
             # add number of fields between queen and lower border or left border, whichever is closer
             fields += min(queen_lower_border_distance, queen_left_border_distance)
